89321c66810147089b8972de0901883f1db0d149
ramiIbra7im <abrahymramy19@gmail.com>
2025-09-18 21:33:07 +0300

    Update3.2

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Noto_Kufi_Arabic } from 'next/font/google';
 import { AuthProvider } from "./context/AuthContext";
 import { ToastContainer } from 'react-toastify';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 
 const notoKufi = Noto_Kufi_Arabic({
